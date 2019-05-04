2b1960a1a3087ef125fce59c5d857b84abffb3b2
ninrod <filipe.silva@gmail.com>
2019-05-04 16:00:19 -0300

    implement 2 way binding

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,7 +5,11 @@ const person = (props) => {
     <div>
       <h1>I'm {props.name}. I'm {props.age} years old</h1>
       <p>{props.children}</p>
-      <input type="text" onChange={props.change}/>
+      <input
+        type="text"
+        onChange={props.change}
+        defaultValue={props.name}
+      />
     </div>
   );
 };
