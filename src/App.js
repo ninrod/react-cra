23ec721d05a03642989b00b517634529e17a9972
ninrod <filipe.silva@gmail.com>
2019-05-03 23:44:07 -0300

    add a click listener

--- src/App.js
+++ src/App.js
@@ -9,11 +9,17 @@ class App extends Component {
       {name: 'filipe', age: 38},
       {name: 'juliana', age: 4}
     ]
-  }
+  };
+
+  clickHandler = () => {
+    console.log('funfou');
+  };
+
   render() {
     return (
       <div className="App">
         <h1>hi, I'm a react app</h1>
+        <button onClick={this.clickHandler}>clica!</button>
         <Person age={this.state.persons[0].age}  name={this.state.persons[0].name}/>
         <Person age={this.state.persons[1].age}  name={this.state.persons[1].name}/>
         <Person age={this.state.persons[2].age}  name={this.state.persons[2].name}>
